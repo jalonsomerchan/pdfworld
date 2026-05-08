792246b8bbf82ad69692d08613f5bb944d5d4a03
Jorge Alonso <jalonsomerchan@gmail.com>
2026-05-08 08:34:56 +0200

    Añade sitemap bilingüe

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,28 @@
+import { locales } from '../i18n/ui';
+import { getLocalizedPath } from '../i18n/utils';
+
+const pages = ['', 'unir-pdf'];
+
+export async function GET({ site }: { site: URL }) {
+  const urls = pages.flatMap((page) =>
+    locales.map((lang) => new URL(getLocalizedPath(lang, page), site).toString()),
+  );
+
+  return new Response(
+    `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls
+  .map(
+    (url) => `  <url>
+    <loc>${url}</loc>
+  </url>`,
+  )
+  .join('\n')}
+</urlset>`,
+    {
+      headers: {
+        'Content-Type': 'application/xml',
+      },
+    },
+  );
+}
