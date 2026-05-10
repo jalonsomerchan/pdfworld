d2fe648e173359b06b20ec45be6058958b925f41
Jorge Alonso <jalonsomerchan@gmail.com>
2026-05-10 13:32:52 +0200

    seo: harden robots endpoint

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,16 +1,20 @@
-export async function GET({ site }: { site: URL }) {
-  const sitemap = new URL('/sitemap.xml', site).toString();
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = ({ site }) => {
+  const origin = site ?? new URL('https://pdfworld.alon.one');
+  const sitemap = new URL('/sitemap.xml', origin).toString();
 
   return new Response(
     `User-agent: *
 Allow: /
+Disallow: /sw.js
 
 Sitemap: ${sitemap}
 `,
     {
       headers: {
-        'Content-Type': 'text/plain',
+        'Content-Type': 'text/plain; charset=utf-8',
       },
     },
   );
-}
+};
