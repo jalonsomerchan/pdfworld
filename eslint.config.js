ddff118fa5199950cb9c41856df443fa9cdd5984
Jorge Alonso <jalonsomerchan@gmail.com>
2026-05-11 08:57:46 +0200

    ci: configure eslint for svelte typescript

--- eslint.config.js
+++ eslint.config.js
@@ -4,9 +4,8 @@ import tseslint from 'typescript-eslint';
 import astro from 'eslint-plugin-astro';
 import svelte from 'eslint-plugin-svelte';
 
-const astroParserOptions = {
+const parserOptions = {
   parser: tseslint.parser,
-  extraFileExtensions: ['.astro'],
 };
 
 export default tseslint.config(
@@ -29,14 +28,25 @@ export default tseslint.config(
     },
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-expressions': 'off',
       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      'no-empty': 'off',
       'svelte/no-at-html-tags': 'off',
+    },
+  },
+  {
+    files: ['**/*.svelte'],
+    languageOptions: {
+      parserOptions,
     },
   },
   {
     files: ['**/*.astro'],
     languageOptions: {
-      parserOptions: astroParserOptions,
+      parserOptions: {
+        ...parserOptions,
+        extraFileExtensions: ['.astro'],
+      },
     },
   },
 );
